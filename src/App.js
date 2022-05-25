07d4756e071ea1e3f0167d68f27893c47cbc8c6b
M Waseem <wasirana80@gmail.com>
2022-05-25 18:38:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import { useEffect } from "react";
 
 const id = "240522";
 function App() {
-  axios.defaults.baseURL = "http://localhost:9000";
+  axios.defaults.baseURL = "http://15.207.39.69:9000";
   useEffect(() => {
     if (
       localStorage.getItem("user_uuid") &&
