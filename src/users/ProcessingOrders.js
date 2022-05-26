a20ded6576003865b7057f5b5561dcb1901dab85
M Waseem <wasirana80@gmail.com>
2022-05-26 18:12:29 +0500

    Update ProcessingOrders.js

--- src/users/ProcessingOrders.js
+++ src/users/ProcessingOrders.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/solid";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { openDB } from "idb";
 const ProcessingOrders = () => {
   const [orders, setOrders] = useState([]);
   const params = useParams();
-  const [items, setItems] = useState("sort_order");
-  const [order, setOrder] = useState("");
+  const [items,setItems]=useState([])
+  const [selectedOrder, setSelectedOrder] = useState();
+  const getIndexedDbData = async () => {
+    const db = await openDB("BT", +localStorage.getItem("IDBVersion") || 1);
+    let tx = await db.transaction("items", "readwrite").objectStore("items");
+    let item = await tx.getAll();
+    setItems(item);
+}
   const getTripOrders = async () => {
     const response = await axios({
       method: "post",
@@ -20,12 +27,47 @@ const ProcessingOrders = () => {
   };
   useEffect(() => {
     getTripOrders();
+    getIndexedDbData()
   }, []);
 
   console.log(orders);
   return (
-    <div className="item-sales-container orders-report-container" style={{width:"100%",left:"0",top:"0"}}>
-      <div className="table-container-user item-sales-container" style={{width:"100%",left:"0",top:"0"}}>
+    <div
+      className="item-sales-container orders-report-container"
+      style={{ width: "100%", left: "0", top: "0", textAlign: "center" }}
+    >
+      {selectedOrder ? (
+        <>
+          <h1>{selectedOrder.counter_title}</h1>
+          <button
+            className="item-sales-search"
+            style={{ width: "max-content" }}
+          >
+            Next
+          </button>
+        </>
+      ) : (
+        ""
+      )}
+      <div
+        className="table-container-user item-sales-container"
+        style={{ width: "100%", left: "0", top: "0", display: "flex" }}
+      >
+        {selectedOrder ? (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "10px",
+            }}
+          >
+            <AiFillPlayCircle style={{ fontSize: "30px", cursor: "pointer" }} />
+          </div>
+        ) : (
+          ""
+        )}
+
         <table
           className="user-table"
           style={{
@@ -37,36 +79,57 @@ const ProcessingOrders = () => {
           <thead>
             <tr>
               <th>S.N</th>
-              <th colSpan={2}>
-                <div className="t-head-element">
-              Counter Title
-                  
-                </div>
-              </th>
-              <th colSpan={2}>
-                {" "}
-                <div className="t-head-element">
-                  Progress
-                </div>
-              </th>
+              {selectedOrder ? (
+                <>
+                  <th colSpan={2}>
+                    <div className="t-head-element">Item Name</div>
+                  </th>
+                  <th >
+                    <div className="t-head-element">MRP</div>
+                  </th>
+                  <th >
+                    <div className="t-head-element">Qty</div>
+                  </th>
+                </>
+              ) : (
+                <>
+                  <th colSpan={2}>
+                    <div className="t-head-element">Counter Title</div>
+                  </th>
+                  <th colSpan={2}>
+                    <div className="t-head-element">Progress</div>
+                  </th>
+                </>
+              )}
             </tr>
           </thead>
           <tbody className="tbody">
-            {orders
-              .sort((a, b) =>
-                order === "asc"
-                  ? typeof a[items] === "string"
-                    ? a[items].localeCompare(b[items])
-                    : a[items] - b[items]
-                  : typeof a[items] === "string"
-                  ? b[items].localeCompare(a[items])
-                  : b[items] - a[items]
-              )
+            {selectedOrder?
+            selectedOrder.item_details
+            ?.map((item, i) => (
+              <tr
+                key={Math.random()}
+                style={{ height: "30px" }}
+                onClick={() => setSelectedOrder(item)}
+              >
+                <td>{i + 1}</td>
+                <td colSpan={2}>{items.find(a=>a.item_uuid===item.item_uuid)?.item_title}</td>
+                <td >{items.find(a=>a.item_uuid===item.item_uuid)?.mrp}</td>
+                <td >{item.b+":"+item.p}</td>
+              </tr>
+            ))
+            
+            : orders
+              .sort((a, b) => a.created_at - b.created_at)
               ?.map((item, i) => (
-                <tr key={Math.random()} style={{ height: "30px" }}>
+                <tr
+                  key={Math.random()}
+                  style={{ height: "30px" }}
+                  onClick={() => setSelectedOrder(item)}
+                >
                   <td>{i + 1}</td>
                   <td colSpan={2}>{item.counter_title}</td>
-                  <td colSpan={2}>0/{item?.item_details?.length||0}</td>
+                  <td colSpan={2}>0/{item?.item_details?.length || 0}</td>
                 </tr>
               ))}
           </tbody>
