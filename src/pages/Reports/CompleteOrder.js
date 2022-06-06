c2b20b093f6c75cb22a38a21c258327178023ed4
M Waseem <wasirana80@gmail.com>
2022-06-06 16:52:17 +0500

    Update CompleteOrder.js

--- src/pages/Reports/CompleteOrder.js
+++ src/pages/Reports/CompleteOrder.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
+import { OrderDetails } from "../../components/OrderDetails";
 import Sidebar from "../../components/Sidebar";
 const CompleteOrder = () => {
   const [searchData, setSearchData] = useState({
@@ -95,7 +96,7 @@ const CompleteOrder = () => {
         </div>
       </div>
      {popupOrder ? (
-        <AddOrder
+        <OrderDetails
           onSave={() => {
             setPopupOrder(null);
           }}
@@ -160,134 +161,5 @@ function Table({ itemsDetails,setPopupOrder }) {
           ))}
       </tbody>
     </table>
-  );
-}
-function AddOrder({ order, onSave }) {
-  const [counters, setCounters] = useState([]);
-  const [itemsData, setItemsData] = useState([]);
-console.log(order)
-  const getItemsData = async () => {
-    const response = await axios({
-      method: "get",
-      url: "/items/GetItemList",
-
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.data.success) setItemsData(response.data.result);
-  };
-
-  const getCounter = async () => {
-    const response = await axios({
-      method: "get",
-      url: "/counters/GetCounterList",
-
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.data.success) setCounters(response.data.result);
-  };
-  useEffect(() => {
-    getCounter();
-    getItemsData();
-  }, []);
-
-  return (
-    <>
-      <div className="overlay">
-        <div className="modal" style={{ height: "fit-content", width: "80vw" }}>
-          <div
-            className="content"
-            style={{
-              height: "fit-content",
-              padding: "20px",
-              width: "fit-content",
-            }}
-          ></div>
-          <div className="inventory">
-            <div
-              className="accountGroup"
-              id="voucherForm"
-              action=""
-              style={{
-                height: "max-content",
-                maxHeight: "500px",
-                overflow: "scroll",
-              }}
-            >
-              <div className="inventory_header">
-                <h2>Order Details</h2>
-              </div>
-
-              <div className="topInputs">
-                <div className="inputGroup">
-                  <h2>
-                    {counters.find((a) => a.counter_uuid === order.counter_uuid)
-                      ?.counter_title || ""}
-                  </h2>
-                </div>
-              </div>
-
-              <div
-                className="items_table"
-                style={{ flex: "1", paddingLeft: "10px" }}
-              >
-                <table className="f6 w-100 center" cellSpacing="0">
-                  <thead className="lh-copy" style={{ position: "static" }}>
-                    <tr className="white">
-                      <th className="pa2 tl bb b--black-20 w-30">Item Name</th>
-                      <th className="pa2 tc bb b--black-20">Quantity(b)</th>
-                      <th className="pa2 tc bb b--black-20">Quantity(p)</th>
-                      <th className="pa2 tc bb b--black-20 ">Price</th>
-                    </tr>
-                  </thead>
-                  
-                    <tbody className="lh-copy">
-                      {order?.item_details?.map((item, i) => {
-                        return (
-                          <tr key={i} style={{height:"50px"}}>
-                            <td className="ph2 pv1 tl bb b--black-20 bg-white">
-                              <div className="inputGroup">
-                                {itemsData.find(
-                                  (a) => a.item_uuid === item.item_uuid
-                                )?.item_title || ""}
-                              </div>
-                            </td>
-                            <td
-                              className="ph2 pv1 tc bb b--black-20 bg-white"
-                              style={{ textAlign: "center" }}
-                            >
-                              {item.b || 0}
-                            </td>
-                            <td
-                              className="ph2 pv1 tc bb b--black-20 bg-white"
-                              style={{ textAlign: "center" }}
-                            >
-                              {item.p || 0}
-                            </td>
-                            <td
-                              className="ph2 pv1 tc bb b--black-20 bg-white"
-                              style={{ textAlign: "center" }}
-                            >
-                              Rs {item?.item_price || 0}
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                 
-                </table>
-              </div>
-              <div className="bottomContent"></div>
-            </div>
-            <button onClick={onSave} className="closeButton">
-              x
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
   );
 }
