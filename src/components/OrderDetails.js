d90b9a2ea9d0ea06c6303635b8a339743a86ce83
M Waseem <wasirana80@gmail.com>
2022-06-11 13:33:36 +0500

    Update OrderDetails.js

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -717,13 +717,17 @@ export function OrderDetails({ order, onSave, orderStatus }) {
           )}
           order={order}
           date={new Date(order.status[0].time)}
-          user={users.find(a=>a.user_uuid===order.status[0].user_uuid)?.user_title||""}
+          user={
+            users.find((a) => a.user_uuid === order.status[0].user_uuid)
+              ?.user_title || ""
+          }
+          itemData={itemsData}
         />
       </div>
     </>
   );
 }
-const OrderPrint = ({ counter, order, date, user }) => {
+const OrderPrint = ({ counter, order, date, user, itemData }) => {
   return (
     <>
       <table style={{ borderBottom: "1px solid black", width: "100%" }}>
@@ -808,12 +812,185 @@ const OrderPrint = ({ counter, order, date, user }) => {
               )
               .replace("dd", ("00" + date?.getDate().toString()).slice(-2))}
           </td>
-          <td style={{ fontSize: "x-small" }}>
-            S.M: {user}
-          </td>
+          <td style={{ fontSize: "x-small" }}>S.M: {user}</td>
           <td style={{ fontSize: "x-small" }}>Memo: Cash</td>
         </tr>
       </table>
+      <div style={{ height: "50%", borderBottom: "1px solid black" }}>
+        <table
+          style={{
+            width: "100%",
+          }}
+        >
+          <tr
+            style={{
+              backgroundColor: "#EDEDED",
+            }}
+          >
+            <th style={{ fontSize: "x-small" }}>S.</th>
+            <th style={{ fontSize: "x-small" }} colSpan={3}>
+              Product
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              MRP
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Qty
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Free
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Tax (%)
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Unit Price
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Dsc A (%)
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Dsc B (%)
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Dsc Amt
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Tex Amt
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Net Unit Price
+            </th>
+            <th style={{ fontSize: "x-small" }} colSpan={2}>
+              Amount
+            </th>
+          </tr>
+
+          {order.item_details.map((item, i) => {
+            const itemInfo = itemData.find(
+              (a) => a.item_uuid === item.item_uuid
+            );
+            let itemQty =
+              (+item.q || 0) * (+itemInfo?.conversion || 1) + (+item.p || 0);
+            let unit_price = (+item.item_total || 0) / (+itemQty || 1);
+            let tex_amt =
+              (+unit_price || 0) -
+              ((+unit_price || 0) * 100) / (100 + (+item.gst_percentage || 0));
+            let dsc_amt = (+unit_price || 0) - +item.price;
+            return (
+              <tr style={{ borderBottom: "1px solid #000" }}>
+                <td style={{ fontSize: "x-small" }}>{i + 1}</td>
+                <td style={{ fontSize: "x-small" }} colSpan={3}>
+                  {itemInfo?.item_title || ""}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {itemInfo?.mrp || ""}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {(item.b || 0) + ":" + ("00" + item?.p.toString()).slice(-2)}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {item?.free || 0}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {item?.gst_percentage || 0}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {item?.price || 0}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {item.charges_discount.length
+                    ? item.charges_discount[0].value || 0
+                    : 0}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {item.charges_discount.length > 1
+                    ? item.charges_discount[1].value || 0
+                    : 0}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {(dsc_amt || 0).toFixed(2)}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {(tex_amt || 0).toFixed(2)}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {(unit_price || 0).toFixed(2)}
+                </td>
+                <td
+                  style={{
+                    fontSize: "x-small",
+                    textAlign: "center",
+                  }}
+                  colSpan={2}
+                >
+                  {item?.item_total || 0}
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </>
   );
 };
