5f72cb4c9868f49644d286a89ce1c8d345540994
M Waseem <wasirana80@gmail.com>
2022-06-14 15:33:23 +0500

    Update OrderPrint.js

--- src/components/OrderPrint.js
+++ src/components/OrderPrint.js
@@ -18,7 +18,7 @@ const OrderPrint = ({
     let tex_amt =
       (+unit_price || 0) -
       ((+unit_price || 0) * 100) / (100 + (+item.gst_percentage || 0));
-    let dsc_amt = (+unit_price || 0) - +item.price;
+    let dsc_amt =  ( +item.price-(+unit_price || 0))*itemQty;
     return { dsc_amt, tex_amt };
   });
   return (
@@ -196,7 +196,7 @@ const OrderPrint = ({
           let tex_amt =
             (+unit_price || 0) -
             ((+unit_price || 0) * 100) / (100 + (+item.gst_percentage || 0));
-          let dsc_amt = (+unit_price || 0) - +item.price;
+          let dsc_amt =   (+item.price-(+unit_price || 0))*itemQty;
           return (
             <tr
               style={{ borderBottom: "1px solid #000" }}
