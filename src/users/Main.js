229384cf5ef5a125f0c2aaa2fc6462c1df22699d
M Waseem <wasirana80@gmail.com>
2022-06-23 17:07:02 +0500

    Update Main.js

--- src/users/Main.js
+++ src/users/Main.js
@@ -73,7 +73,7 @@ const Main = () => {
           ))}
         </div>
         <div style={{ position: "fixed", bottom: "60px",right:"20vw",fontSize:"20px" }}>
-          Version 11
+          Version 12
         </div>
         <button
           type="button"
