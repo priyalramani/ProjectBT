6c2a02e4ed768fe16bffbdfc9f8075fc024f4c62
M Waseem <wasirana80@gmail.com>
2022-08-18 22:39:16 +0500

    Update CurrentStock.js

--- src/pages/Reports/CurrentStock.js
+++ src/pages/Reports/CurrentStock.js
@@ -4,6 +4,9 @@ import Sidebar from "../../components/Sidebar";
 import Headers from "../../components/Header";
 const CurrentStock = () => {
   const [itemsData, setItemsData] = useState([]);
+  const [filterTitle, setFilterTitle] = useState("");
+  const [itemEditPopup, setItemEditPopup] = useState("");
+  const [item, setItem] = useState("");
 
   const [warehouseData, setWarehouseData] = useState([]);
 
@@ -47,18 +50,73 @@ const CurrentStock = () => {
         <div id="heading" className="flex">
           <h2 style={{ width: "70%" }}>Current Stock</h2>
         </div>
+        <div id="item-sales-top">
+          <div
+            id="date-input-container"
+            style={{
+              overflow: "visible",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <input
+              type="text"
+              onChange={(e) => setFilterTitle(e.target.value)}
+              value={filterTitle}
+              placeholder="Search Item Title..."
+              className="searchInput"
+            />
 
+            <div>
+              Total Items:{" "}
+              {
+                itemsData.filter(
+                  (a) =>
+                    !filterTitle ||
+                    a.item_title
+                      .toLocaleLowerCase()
+                      .includes(filterTitle.toLocaleLowerCase())
+                ).length
+              }
+            </div>
+          </div>
+        </div>
         <div className="table-container-user item-sales-container">
-          <Table itemsDetails={itemsData} warehouseData={warehouseData} />
+          <Table
+            itemsDetails={itemsData.filter(
+              (a) =>
+                !filterTitle ||
+                a.item_title
+                  .toLocaleLowerCase()
+                  .includes(filterTitle.toLocaleLowerCase())
+            )}
+            setItemData={setItem}
+            setItemEditPopup={setItemEditPopup}
+            warehouseData={warehouseData}
+          />
         </div>
       </div>
+      {itemEditPopup ? (
+        <QuantityChanged
+          popupInfo={itemEditPopup}
+          item={item}
+          onSave={() => {
+            setItemEditPopup("");
+          }}
+          update={getItemsData}
+        />
+      ) : (
+        ""
+      )}
     </>
   );
 };
 
 export default CurrentStock;
 
-function Table({ itemsDetails, warehouseData }) {
+function Table({ itemsDetails, warehouseData, setItemEditPopup, setItemData }) {
   const CovertedQty = (qty, conversion) => {
     let b = (qty / +conversion).toFixed(0);
 
@@ -91,16 +149,38 @@ function Table({ itemsDetails, warehouseData }) {
               </td>
 
               <td colSpan={2}>{item.item_title || ""}</td>
-              {warehouseData.map((a) => (
-                <td>
-                  {CovertedQty(
-                    item?.stock?.find(
-                      (b) => b.warehouse_uuid === a.warehouse_uuid
-                    )?.qty || 0,
-                    item.conversion
-                  )}
-                </td>
-              ))}
+              {warehouseData.map((a) => {
+                let data = item?.stock?.find(
+                  (b) => b.warehouse_uuid === a.warehouse_uuid
+                );
+                return (
+                  <td
+                    className="flex"
+                    style={{ justifyContent: "space-between" }}
+                  >
+                    <span
+                      style={{ cursor: "pointer" }}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setItemEditPopup({ type: "qty", ...a });
+                        setItemData(item);
+                      }}
+                    >
+                      {CovertedQty(data?.qty || 0, item.conversion)}
+                    </span>
+                    <span
+                      style={{ cursor: "pointer" }}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setItemEditPopup({ type: "min_level", ...a });
+                        setItemData(item);
+                      }}
+                    >
+                      ({data?.min_level || 0})
+                    </span>
+                  </td>
+                );
+              })}
               <td>
                 {CovertedQty(
                   item?.stock?.length > 1
@@ -118,5 +198,178 @@ function Table({ itemsDetails, warehouseData }) {
           ))}
       </tbody>
     </table>
+  );
+}
+function QuantityChanged({ onSave, popupInfo, item, update }) {
+  const [data, setdata] = useState({});
+
+  useEffect(() => {
+    let warehouseData = item.stock?.find(
+      (a) => a.warehouse_uuid === popupInfo.warehouse_uuid
+    );
+
+    if (warehouseData)
+      setdata({
+        ...warehouseData,
+        b: (+warehouseData.qty / +item.conversion).toFixed(0),
+        p: (+warehouseData.qty % +item.conversion).toFixed(0),
+      });
+    else
+      setdata({
+        warehouse_uuid: popupInfo.warehouse_uuid,
+        b: 0,
+        p: 0,
+        min_level: 0,
+      });
+  }, [item.conversion, item.stock, popupInfo.warehouse_uuid]);
+  console.log(data);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    let qty = +(+data.b * +item.conversion) + +data.p;
+    let stock = item.stock;
+    stock = stock?.filter((a) => a.warehouse_uuid === popupInfo.warehouse_uuid)
+      ?.length
+      ? stock.map((a) =>
+          a.warehouse_uuid === popupInfo.warehouse_uuid ? { ...data, qty } : a
+        )
+      : stock?.length
+      ? +popupInfo.warehouse_uuid === 0
+        ? stock || []
+        : [
+            ...stock,
+            {
+              ...data,
+              qty,
+            },
+          ]
+      : [
+          {
+            ...data,
+            qty,
+          },
+        ];
+    console.log(stock);
+    const response = await axios({
+      method: "put",
+      url: "/items/putItem",
+      data: [{ item_uuid: item.item_uuid, stock }],
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.data.result[0].success) {
+      onSave();
+      update();
+    }
+  };
+
+  return (
+    <div className="overlay">
+      <div
+        className="modal"
+        style={{ height: "fit-content", width: "max-content" }}
+      >
+        <div
+          className="content"
+          style={{
+            height: "fit-content",
+            padding: "20px",
+            width: "fit-content",
+          }}
+        >
+          <div style={{ overflowY: "scroll" }}>
+            <form className="form" onSubmit={submitHandler}>
+              <div className="formGroup">
+                {popupInfo.type === "qty" ? (
+                  <div
+                    className="row"
+                    style={{ flexDirection: "row", alignItems: "flex-start" }}
+                  >
+                    <label
+                      className="selectLabel flex"
+                      style={{ width: "100px" }}
+                    >
+                      Box
+                      <input
+                        type="number"
+                        name="route_title"
+                        className="numberInput"
+                        value={data.b}
+                        style={{ width: "100px" }}
+                        onChange={(e) =>
+                          setdata({
+                            ...data,
+                            b: e.target.value,
+                          })
+                        }
+                        maxLength={42}
+                        onWheel={(e) => e.preventDefault()}
+                      />
+                      {popupInfo.conversion || 0}
+                    </label>
+                    <label
+                      className="selectLabel flex"
+                      style={{ width: "100px" }}
+                    >
+                      Pcs
+                      <input
+                        type="number"
+                        name="route_title"
+                        className="numberInput"
+                        value={data.p}
+                        style={{ width: "100px" }}
+                        onChange={(e) =>
+                          setdata({
+                            ...data,
+                            p: e.target.value,
+                          })
+                        }
+                        maxLength={42}
+                        onWheel={(e) => e.preventDefault()}
+                        autoFocus={true}
+                      />
+                    </label>
+                  </div>
+                ) : (
+                  <div
+                    className="row"
+                    style={{ flexDirection: "row", alignItems: "flex-start" }}
+                  >
+                    <label
+                      className="selectLabel flex"
+                      style={{ width: "100px" }}
+                    >
+                      Min Level
+                      <input
+                        type="number"
+                        name="route_title"
+                        className="numberInput"
+                        value={data?.min_level}
+                        style={{ width: "100px" }}
+                        onChange={(e) =>
+                          setdata({
+                            ...data,
+                            min_level: e.target.value,
+                          })
+                        }
+                        maxLength={42}
+                        onWheel={(e) => e.preventDefault()}
+                      />
+                    </label>
+                  </div>
+                )}
+              </div>
+
+              <button type="submit" className="submit">
+                Save changes
+              </button>
+            </form>
+          </div>
+          <button onClick={onSave} className="closeButton">
+            x
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
