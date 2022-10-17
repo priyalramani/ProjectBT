f8c96a8710b724b08f39e769dbe79cdc2eb06f0d
M Waseem <wasirana80@gmail.com>
2022-10-17 10:30:35 +0500

    Update MessagePopup.js

--- src/components/MessagePopup.js
+++ src/components/MessagePopup.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const MessagePopup = ({ onClose, message, onSave, button1, button2 }) => {
+const MessagePopup = ({ onClose, message,message2, onSave, button1, button2 }) => {
   return (
-    <div className="overlay" style={{position:"fixed",top:0,left:0,zIndex:9999999}}>
+    <div className="overlay" style={{position:"fixed",top:0,left:0,zIndex:9999999999}}>
       <div
         className="modal"
         style={{ height: "fit-content", width: "fit-content" }}
@@ -24,8 +24,9 @@ const MessagePopup = ({ onClose, message, onSave, button1, button2 }) => {
               }}
             >
               <div className="formGroup">
-                <div className="row">
+                <div className="row" style={{flexDirection:"column",alignItems:"start"}}>
                   <h1 style={{ textAlign: "center" }}>{message}</h1>
+                  {message2?<h3 style={{ textAlign: "center" }}>{message2}</h3>:""}
                 </div>
 
                 <div className="row">
