c1740316bd38852568aafdc58b05eb82d3442806
Jatin Sharma <jatin2907sharma@gmail.com>
2023-05-05 10:42:56 +0530

    v103

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ import OrderPdf from "./components/OrderPdf"
 import MobileLayout from "./components/MobileLayout"
 import ItemAvilibility from "./pages/QuikAccess/ItemAvilibility"
 import CashRegister from "./pages/QuikAccess/CashRegister"
-export let Version = 102
+export let Version = 103
 // export const server = "http://localhost:9000"
 export const server = "https://api.btgondia.com"
 function App() {
